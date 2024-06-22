70605bfb5d6c51d03f426f7942d12f177f468b18
daniel2887 <daniels2887@gmail.com>
2024-06-22 15:34:48 -0700

    Move build_corne.bat to the zmk firmware fork

--- build_corne.bat
+++
@@ -1,8 +0,0 @@
-@echo off
-pushd ..\zmk\app
-west build -d build/left -b nice_nano_v2 -- -DSHIELD=corne_left || echo Error building Corne left && popd && exit /b
-west build -d build/right -b nice_nano_v2 -- -DSHIELD=corne_right || echo Error building Corne right && popd && exit /b
-popd
-if not exist "output" mkdir output
-copy ..\zmk\app\build\left\zephyr\zmk.uf2 output\left.uf2 || echo Error copying right firmware && exit /b
-copy ..\zmk\app\build\right\zephyr\zmk.uf2 output\right.uf2 || echo Error copying left firmware && exit /b
